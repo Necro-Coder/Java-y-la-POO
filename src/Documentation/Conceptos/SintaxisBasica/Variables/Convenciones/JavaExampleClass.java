cc4ee995252e4961b39426b5b0feb513d12a35b4
= <=>
2024-01-31 09:51:26 +0100

    Commit antes de empezar con los arrays

---
+++ src/Documentation/Conceptos/SintaxisBasica/Variables/Convenciones/JavaExampleClass.java
@@ -0,0 +1,6 @@
+package Documentation.Conceptos.SintaxisBasica.Variables.Convenciones;
+
+public class JavaExampleClass {
+    public static final String JDK_VERSION = "1.8";
+    public String javaName = "Java";
+}
